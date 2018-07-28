2eae47cdce89c1ed3887255f955469fa14a42bf1
zonozono32 <zonocat32@gmail.com>
2018-07-28 21:28:31 +0900

    各テーブルのマイグレーションファイルの作成

--- database/migrations/2018_07_28_120459_create_purchase_histories_table.php
+++ database/migrations/2018_07_28_120459_create_purchase_histories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePurchaseHistoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('user_id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('purchase_histories', function (Blueprint $table) {
+            $table->increments('purchase_id');
+            $table->unsignedInteger('capsule_id');
+            $table->unsignedInteger('user_id');
+            $table->integer('quantity')->default(1);
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('purchase_histories');
     }
 }
