db9eb0c8d71122dfabc078c8fe07d77cb1d2aa77
zonozono32 <zonocat32@gmail.com>
2018-08-05 21:42:07 +0900

    カプセルのミルク可否をミルクの目盛量に変更

--- database/migrations/2018_07_28_120324_create_capsules_table.php
+++ database/migrations/2018_07_28_120324_create_capsules_table.php
@@ -17,8 +17,8 @@ class CreateCapsulesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->text('description');
-            $table->boolean('use_milk');
             $table->integer('optimal_scale');
+            $table->integer('milk_scale');
             $table->integer('price');
             $table->timestamps();
         });
