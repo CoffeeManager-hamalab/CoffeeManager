81a998259601d4ff45f5cd989d817f76039981c4
zonozono32 <zonocat32@gmail.com>
2018-08-19 21:22:09 +0900

    ログインリダイレクトの実装

--- app/Http/Controllers/PurchaseHistoryController.php
+++ app/Http/Controllers/PurchaseHistoryController.php
@@ -11,6 +11,16 @@ use App\PurchaseHistory;
 
 class PurchaseHistoryController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index() {
         $user = Auth::user();
         $history = DB::table('purchase_histories')
