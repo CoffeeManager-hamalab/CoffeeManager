cb64512f1aec4455cd314336219d42a11df5bcd0
zonozono32 <zonocat32@gmail.com>
2018-08-27 00:05:58 +0900

    日付ごとに購入履歴を出力するように変更

--- app/Http/Controllers/PurchaseHistoryController.php
+++ app/Http/Controllers/PurchaseHistoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 use App\Capsule;
 use App\PurchaseHistory;
@@ -25,10 +26,29 @@ class PurchaseHistoryController extends Controller
         $user = Auth::user();
         $history = DB::table('purchase_histories')
                     ->join('capsules', 'purchase_histories.capsule_id', '=', 'capsules.id')
-                    ->select('capsules.name', 'capsules.price', DB::raw('sum(purchase_histories.quantity) as sum_quantity'))
+                    ->select('capsules.name', 'capsules.price', DB::raw('sum(purchase_histories.quantity) as sum_quantity'), DB::raw('date_format(purchase_histories.created_at, "%Y%m%d") as date'))
                     ->where('purchase_histories.user_id', '=', $user->id)
-                    ->groupBy('capsules.id')
+                    ->groupBy('capsules.id', DB::raw('date_format(purchase_histories.created_at, "%Y%m%d")'))
+                    ->latest('date')
                     ->get();
-        return view('purchase-history.index', ['history' => $history]);
+
+        $group_date = new Carbon(Carbon::now());
+        $ret = [];
+        $tmp = [];
+        foreach ($history as $data) {
+            $carbon_date = new Carbon($data->date);
+            if (!$group_date->isSameDay($carbon_date)) {
+                $ret[] = $tmp;
+                $group_date = new Carbon($data->date);
+                $tmp = [
+                    'purchase' => [],
+                    'date' => $data->date
+                ];
+            }
+            $tmp['purchase'][] = $data;
+        }
+        $ret[] = $tmp;
+        array_shift($ret);
+        return view('purchase-history.index', ['histories' => $ret]);
     }
 }
