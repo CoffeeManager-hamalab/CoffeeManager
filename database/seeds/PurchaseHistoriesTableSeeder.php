02a6718a0fa544d4f296fd34b85eb166db23335e
zonozono32 <zonocat32@gmail.com>
2018-08-26 21:07:12 +0900

    購入履歴の日付を編集

--- database/seeds/PurchaseHistoriesTableSeeder.php
+++ database/seeds/PurchaseHistoriesTableSeeder.php
@@ -2,47 +2,10 @@
 
 use Illuminate\Database\Seeder;
 use App\PurchaseHistory;
+use Carbon\Carbon;
 
 class PurchaseHistoriesTableSeeder extends Seeder
 {
-    private $seeds = [
-        [
-            'capsule_id' => 1,
-            'user_id' => 1,
-            'quantity' => 1
-        ],
-        [
-            'capsule_id' => 1,
-            'user_id' => 2,
-            'quantity' => 1
-        ],
-        [
-            'capsule_id' => 2,
-            'user_id' => 2,
-            'quantity' => 1
-        ],
-        [
-            'capsule_id' => 3,
-            'user_id' => 2,
-            'quantity' => 1
-        ],
-        [
-            'capsule_id' => 4,
-            'user_id' => 2,
-            'quantity' => 3
-        ],
-        [
-            'capsule_id' => 2,
-            'user_id' => 1,
-            'quantity' => 1
-        ],
-        [
-            'capsule_id' => 1,
-            'user_id' => 1,
-            'quantity' => 1
-        ]
-    ];
-
     /**
      * Run the database seeds.
      *
@@ -50,11 +13,76 @@ class PurchaseHistoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        foreach ($this->seeds as $data) {
+        $seeds = [
+            [
+                'capsule_id' => 1,
+                'user_id' => 1,
+                'quantity' => 1,
+                'date' => Carbon::createFromDate(2018, 8, 1)
+            ],
+            [
+                'capsule_id' => 2,
+                'user_id' => 1,
+                'quantity' => 1,
+                'date' => Carbon::createFromDate(2018, 8, 1)
+            ],
+            [
+                'capsule_id' => 1,
+                'user_id' => 1,
+                'quantity' => 1,
+                'date' => Carbon::createFromDate(2018, 8, 1)
+            ],
+            [
+                'capsule_id' => 1,
+                'user_id' => 2,
+                'quantity' => 1,
+                'date' => Carbon::createFromDate(2018, 8, 1)
+            ],
+            [
+                'capsule_id' => 2,
+                'user_id' => 2,
+                'quantity' => 1,
+                'date' => Carbon::createFromDate(2018, 8, 1)
+            ],
+            [
+                'capsule_id' => 3,
+                'user_id' => 2,
+                'quantity' => 1,
+                'date' => Carbon::createFromDate(2018, 8, 1)
+            ],
+            [
+                'capsule_id' => 4,
+                'user_id' => 2,
+                'quantity' => 3,
+                'date' => Carbon::createFromDate(2018, 8, 1)
+            ],
+            [
+                'capsule_id' => 1,
+                'user_id' => 2,
+                'quantity' => 2,
+                'date' => Carbon::createFromDate(2018, 8, 3)
+            ],
+            [
+                'capsule_id' => 2,
+                'user_id' => 2,
+                'quantity' => 1,
+                'date' => Carbon::createFromDate(2018, 8, 3)
+            ],
+            [
+                'capsule_id' => 1,
+                'user_id' => 2,
+                'quantity' => 1,
+                'date' => Carbon::createFromDate(2018, 8, 4)
+            ]
+        ];
+
+        foreach ($seeds as $data) {
             $history = new PurchaseHistory;
             $history->capsule_id = $data['capsule_id'];
             $history->user_id = $data['user_id'];
             $history->quantity = $data['quantity'];
+            $history->created_at = $data['date'];
+            $history->updated_at = $data['date'];
             $history->save();
         }
     }
