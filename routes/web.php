32aa72e8a114eb8ab64384866f436c7590ff601f
hachi2nd <mamoru.hatanaka@gmail.com>
2018-08-27 00:05:59 +0900

    make purchase page

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/purchase-history', 'PurchaseHistoryController@index');
+
+Route::get('/purchase-page', 'CapsulesController@index');
