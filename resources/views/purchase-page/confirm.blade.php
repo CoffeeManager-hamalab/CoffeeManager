90e8ca650fabb551ce8b1e1630dcde361ebf0406
hachi2nd <mamoru.hatanaka@gmail.com>
2018-11-19 00:47:48 +0900

    corrected the purchase confirm page

--- resources/views/purchase-page/confirm.blade.php
+++ resources/views/purchase-page/confirm.blade.php
@@ -3,20 +3,28 @@
 @section('content')
     <h1>購入確認</h1>
 　
-    <div class="alert alert-primary" role="alert">
-     <h4> {{ $capsule->name }},{{ $capsule->price }}円</h4>
-	<a href="/home" class="btn btn-primary">キャンセル</a>
-　　　<form method="post" action="/confirm">
-	 {{ csrf_field() }}
-	 <input type="hidden" class="form-control" name="id" value="{{ $capsule->id }}">
-      <div class="form-group">
-        <input type="hidden" class="form-control" id="name" name="capsule_name" value="{{ $capsule->name }}">
-      </div>
-      <div class="form-group">
-	 <input type="hidden" class="form-control" name="price" value="{{ $capsule->price }}">
-      </div>
-          <button type="submit" class="btn btn-primary">確定</button>
-      </form>
+   <div class="alert alert-primary" role="alert"  style="display:inline-block;">
+	<div class="card" style="width: 15rem;">
+            <img class="card-img-top" src="{{asset('images/')}}/{{ $capsule->img_name}}" alt="カードの画像">
+		<div class="card-body">
+                    <div align="tright">
+			    <h4 align="right"> {{ $capsule->name }},{{ $capsule->price }}円</h4>
+			    <p class="card-text" align="left">{{ $capsule->description }} </p>
+	　　　              <form method="post" action="/confirm" align="right">
+			     {{ csrf_field() }}
+			     <input type="hidden" class="form-control" name="id" value="{{ $capsule->id }}">
+			      <div class="form-group">
+				<input type="hidden" class="form-control" id="name" name="capsule_name" value="{{ $capsule->name }}">
+			      </div>
+			      <div class="form-group">
+				  <input type="hidden" class="form-control" name="price" value="{{ $capsule->price }}">
+			      </div>
+				  <button type="submit" class="btn btn-primary" >確定</button>
+				  <a href="/purchase-page" class="btn btn-primary">キャンセル</a>
+			      </form>
+		    </div>
+	     </div>
+	</div>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
