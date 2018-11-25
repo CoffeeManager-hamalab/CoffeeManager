39a139ad0d70a727d3ccb7849fda1600dd7ca5f1
hachi2nd <mamoru.hatanaka@gmail.com>
2018-11-25 22:44:09 +0900

    購入確認ページのデザイン修正
    購入物の情報をセンタリング

--- resources/views/purchase-page/confirm.blade.php
+++ resources/views/purchase-page/confirm.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>購入確認</h1>
-　
-   <div class="alert alert-primary" role="alert"  style="display:inline-block;">
+   
+   <h1>購入確認</h1>
+   <div style="text-align:center;">
+   <div class="alert alert-primary" role="alert"  style="display:inline-block;margin: auto;">
 	<div class="card" style="width: 15rem;">
             <img class="card-img-top" src="{{asset('images/')}}/{{ $capsule->img_name}}" alt="カードの画像">
 		<div class="card-body">
@@ -26,7 +27,7 @@
 	     </div>
 	</div>
     </div>
-
+   </div>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
