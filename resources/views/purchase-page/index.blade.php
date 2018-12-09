2acdbed2d4aa881669499413332c9957237c20e5
hawkde <akasatana309week@gmail.com>
2018-12-10 00:07:53 +0900

    purchase-pageデザイン

--- resources/views/purchase-page/index.blade.php
+++ resources/views/purchase-page/index.blade.php
@@ -1,25 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-       	    <div class="card-columns">
+            <div class="carousel" data-flickity='{ "groupCells": true }'>
 	      @foreach ($capsule as $data)
+          <div class="carousel-cell" style="width: 20rem">
     		<div class="card">
-   		 <img class="card-img-top" src="{{asset('images/')}}/{{ $data->img_name}}" alt="Card image cap" style="width:15em ">
-   		   <div class="card-body">
-   		    <h4 class="card-title">{{ $data->name}}</h4>
-		     <p class="card-text">{{ $data->price }}円</p>
-  		   </div>
-		     <div class="card-footer">
-			<a href="/confirm/{{$data->name}}" class="btn btn-primary" style="width:100%">Buy</a>
-		     </div>
-		</div>
-       		@endforeach
- 		</div>
+   		       <img class="card-img-top" src="{{asset('images/')}}/{{ $data->name}}.jpg" alt="Card image cap" style="width:15em ">
+   		          <div class="card-body">
+   		            <h4 class="card-title">{{ $data->name}}</h4>
+		            <p class="card-text">{{ $data->price }}å††</p>
+                  </div>
+                <div class="card-footer">
+                    <a href="/confirm/{{$data->name}}" class="btn btn-primary" style="width:10em">Buy</a>
+                </div>
+            </div>
+          </div>
+          @endforeach
         </div>
-    </div>
-</div>
+
 
 @endsection
