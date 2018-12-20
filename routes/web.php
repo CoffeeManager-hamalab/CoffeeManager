7ec0323fd602c3ce7b644bc42625aeb5201ba435
zonozono32 <zonocat32@gmail.com>
2018-12-20 16:38:19 +0900

    confirmが抜けていたので修正

--- routes/web.php
+++ routes/web.php
@@ -1,49 +1,33 @@
 <?php
 
-    Auth::routes();
+Auth::routes();
 
-     Route::get('/', 'CapsulesController@index');
-       
-  
-    // 全ユーザ
-    Route::group(['middleware' => ['auth', 'can:user-higher']], function () {
-        
-      Route::get('/purchase-page', 'CapsulesController@index')->name('CapsulesController.index');
-      Route::post('/purchase-page', 'CapsulesController@confirm')->name('CapsulesController.confirm');
+Route::get('/', 'CapsulesController@index');
 
-      
-       Route::get('/purchase-history', 'PurchaseHistoryController@index')->name('PurchaseHistoryController.index');
-      Route::post('/purchase-history', 'PurchaseHistoryController@complete')->name('PurchaseHistoryController.complete');
+// 全ユーザ
+Route::group(['middleware' => ['auth', 'can:user-higher']], function () {
 
+    Route::get('/purchase-page', 'CapsulesController@index')->name('CapsulesController.index');
+    Route::post('/purchase-page', 'CapsulesController@confirm')->name('CapsulesController.confirm');
+    Route::get('/confirm/{name}', 'CapsulesController@confirm');
+    Route::post('/confirm', 'PurchaseHistoryController@complete');
 
+    Route::get('/purchase-history', 'PurchaseHistoryController@index')->name('PurchaseHistoryController.index');
+    Route::post('/purchase-history', 'PurchaseHistoryController@complete')->name('PurchaseHistoryController.complete');
+});
 
-
-
-    });
+// 管理者以上
+Route::group(['middleware' => ['auth', 'can:admin-higher']], function () {
 
 
+    Route::get('/charge', 'ChargeController@index')->name('ChargeController.index');
+    Route::post('/charge', 'ChargeController@store')->name('ChargeController.store');
 
-    // 管理者以上
-    Route::group(['middleware' => ['auth', 'can:admin-higher']], function () {
+    Route::get('/import-history', 'ImportHistoryController@index')->name('ImportHistoryController.index');
+    Route::post('/import-history', 'ImportHistoryController@store')->name('ImportHistoryController.store');
+});
 
+// システム管理者のみ
+Route::group(['middleware' => ['auth', 'can:system-only']], function () {
 
-      Route::get('/charge', 'ChargeController@index')->name('ChargeController.index');
-      Route::post('/charge', 'ChargeController@store')->name('ChargeController.store');
-
-      Route::get('/import-history', 'ImportHistoryController@index')->name('ImportHistoryController.index');
-       Route::post('/import-history', 'ImportHistoryController@store')->name('ImportHistoryController.store');
-     
-
-    });
-
-
-
-
-    // システム管理者のみ
-    Route::group(['middleware' => ['auth', 'can:system-only']], function () {
-
-    });
-
-
-
-
+});
